7a209e72d8966653fbad76fe1acbd372692338ee
silfanamaulidya <silfanamaulidyasmkmarhas@gmail.com>
2024-09-19 09:58:22 +0700

    update latihan3

--- lib/page2.dart
+++ lib/page2.dart
@@ -7,8 +7,44 @@ class page2 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('ini halaman 2'),
-      )
-    );
-  }
+      child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                  Row(
+                  children: [
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.red,
+                    ),
+                    Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.green,
+                    ),
+                  ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.blue,
+                      ),
+                      Container(
+                        height: 100,
+                        width: 100,
+                        color: Colors.amber,
+                      ),
+                    ],
+                  ),
+                  ],
+            ),
+            
+        ),
+        );
+    }
 }
+      
+  
+  
